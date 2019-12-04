82b9d8d6e1eb52ca50fe1eb3a138a8ff7bd28f1a
Oskar <oskarcali18@gmail.com>
2019-12-03 23:50:45 -0600

    Add login view
    Vista para ingresar nombre de usuario

---
+++ Criptografia Asimetrica/Views/LogIn.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Criptografia_Asimetrica
+{
+    partial class formLogIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formLogIn));
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtBxName = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitle.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(382, 41);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "CRIPTOGRAFIA ASIMETRICA";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtBxName
+            // 
+            this.txtBxName.Location = new System.Drawing.Point(55, 79);
+            this.txtBxName.Name = "txtBxName";
+            this.txtBxName.Size = new System.Drawing.Size(265, 20);
+            this.txtBxName.TabIndex = 1;
+            this.txtBxName.TextChanged += new System.EventHandler(this.txtBxName_TextChanged);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(144, 118);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(95, 44);
+            this.btnEnter.TabIndex = 2;
+            this.btnEnter.Text = "ENTRAR";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // formLogIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 202);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.txtBxName);
+            this.Controls.Add(this.lblTitle);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "formLogIn";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LogIn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtBxName;
+        private System.Windows.Forms.Button btnEnter;
+    }
+}
+
---
+++ Criptografia Asimetrica/Views/LogIn.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Criptografia_Asimetrica.Views;
+
+namespace Criptografia_Asimetrica
+{
+    public partial class formLogIn : Form
+    {
+        public formLogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void txtBxName_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtBxName.Text))
+            {
+                btnEnter.Enabled = true;
+                return;
+            }
+
+            btnEnter.Enabled = false;
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            formHome home = new formHome(txtBxName.Text);
+
+            Hide();
+            home.ShowDialog(this);
+            Show();
+        }
+    }
+}
