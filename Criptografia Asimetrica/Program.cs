d86065ad15a1fa147b3febd826b8fcd932e0d355
Oskar <oskarcali18@gmail.com>
2019-12-03 23:50:20 -0600

    Update project
    Actualizacion del proyecto

--- Criptografia Asimetrica/Program.cs
+++ Criptografia Asimetrica/Program.cs
@@ -16,7 +16,7 @@ namespace Criptografia_Asimetrica
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new formLogIn());
         }
     }
 }
